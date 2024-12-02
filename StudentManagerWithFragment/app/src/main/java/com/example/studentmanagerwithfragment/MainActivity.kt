3a6ef718391ddd05a3356511c06118c94b6e297c
hungnv150203 <vuhunghs2@gmail.com>
2024-12-02 21:03:50 +0700

    student manage fragment

---
+++ StudentManagerWithFragment/app/src/main/java/com/example/studentmanagerwithfragment/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.studentmanagerwithfragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- StudentManagerWithMenu/app/src/main/java/com/example/studentmanagerwithmenu/StudentModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.studentmanagerwithmenu
-
-data class StudentModel(var studentName: String, var studentId: String)
